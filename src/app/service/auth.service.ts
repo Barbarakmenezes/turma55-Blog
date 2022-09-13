e6ae0e472efdecd5ac2c1cf53109c55b301b2aa3
Barbarakmenezes <barbarakerolaine@hotmail.com>
2022-09-13 17:19:03 -0300

    crudPostagem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -23,6 +23,11 @@ entrar (userLogin : UserLogin) : Observable<UserLogin>{
 cadastrar (user:User) : Observable <User>{
   return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
 }
+
+getByIdUser(id:number) : Observable<User> {
+  return this.http.get<User>(`http://localhost:8080/usuarios/${id}`)
+}
+
 logado() {
   let ok : boolean = false
 
